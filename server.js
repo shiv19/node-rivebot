b7c2de64afcbe4868f9b07269bef1a0f26098f95
Shiv19 <mailshiva19@gmail.com>
2017-10-22 17:56:09 +0530

    added port and ip

--- server.js
+++ server.js
@@ -11,96 +11,99 @@
 //    http://localhost:2001/reply
 
 var express = require("express"),
-	bodyParser = require("body-parser"),
-	RiveScript = require("rivescript");
+bodyParser = require("body-parser"),
+RiveScript = require("rivescript");
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 // Create the bot.
 var bot = new RiveScript();
 bot.loadDirectory("./brain", success_handler, error_handler);
 
 function success_handler (loadcount) {
-	console.log("Load #" + loadcount + " completed!");
+console.log("Load #" + loadcount + " completed!");
 
-	bot.sortReplies();
+bot.sortReplies();
 
-	// Set up the Express app.
-	var app = express();
+// Set up the Express app.
+var app = express();
 
-	// Parse application/json inputs.
-	app.use(bodyParser.json());
-	app.set("json spaces", 4);
+// Parse application/json inputs.
+app.use(bodyParser.json());
+app.set("json spaces", 4);
 
-	// Set up routes.
-	app.post("/reply", getReply);
-	app.get("/", showUsage);
-	app.get("*", showUsage);
+// Set up routes.
+app.post("/reply", getReply);
+app.get("/", showUsage);
+app.get("*", showUsage);
 
-	// Start listening.
-	app.listen(2001, function() {
-		console.log("Listening on http://localhost:2001");
-	});
+// Start listening.
+app.listen(port, ip, function() {
+	console.log('Server running on http://%s:%s', ip, port);
+});
 }
 
 function error_handler (loadcount, err) {
-	console.log("Error loading batch #" + loadcount + ": " + err + "\n");
+console.log("Error loading batch #" + loadcount + ": " + err + "\n");
 }
 
 // POST to /reply to get a RiveScript reply.
 function getReply(req, res) {
-	// Get data from the JSON post.
-	var username = req.body.username;
-	var message  = req.body.message;
-	var vars     = req.body.vars;
+// Get data from the JSON post.
+var username = req.body.username;
+var message  = req.body.message;
+var vars     = req.body.vars;
 
-	// Make sure username and message are included.
-	if (typeof(username) === "undefined" || typeof(message) === "undefined") {
-		return error(res, "username and message are required keys");
-	}
+// Make sure username and message are included.
+if (typeof(username) === "undefined" || typeof(message) === "undefined") {
+	return error(res, "username and message are required keys");
+}
 
-	// Copy any user vars from the post into RiveScript.
-	if (typeof(vars) !== "undefined") {
-		for (var key in vars) {
-			if (vars.hasOwnProperty(key)) {
-				bot.setUservar(username, key, vars[key]);
-			}
+// Copy any user vars from the post into RiveScript.
+if (typeof(vars) !== "undefined") {
+	for (var key in vars) {
+		if (vars.hasOwnProperty(key)) {
+			bot.setUservar(username, key, vars[key]);
 		}
 	}
+}
 
-	// Get a reply from the bot.
-	var reply = bot.reply(username, message, this);
+// Get a reply from the bot.
+var reply = bot.reply(username, message, this);
 
-	// Get all the user's vars back out of the bot to include in the response.
-	vars = bot.getUservars(username);
+// Get all the user's vars back out of the bot to include in the response.
+vars = bot.getUservars(username);
 
-	// Send the JSON response.
-	res.json({
-		"status": "ok",
-		"reply": reply,
-		"vars": vars
-	});
+// Send the JSON response.
+res.json({
+	"status": "ok",
+	"reply": reply,
+	"vars": vars
+});
 }
 
 // All other routes shows the usage to test the /reply route.
 function showUsage(req, res) {
-	var egPayload = {
-		"username": "soandso",
-		"message": "Hello bot",
-		"vars": {
-			"name": "Soandso"
-		}
-	};
-	res.writeHead(200, {"Content-Type": "text/plain"});
-	res.write("Usage: curl -i \\\n");
-	res.write("   -H \"Content-Type: application/json\" \\\n");
-	res.write("   -X POST -d '" + JSON.stringify(egPayload) + "' \\\n");
-	res.write("   http://localhost:2001/reply");
-	res.end();
+var egPayload = {
+	"username": "soandso",
+	"message": "Hello bot",
+	"vars": {
+		"name": "Soandso"
+	}
+};
+res.writeHead(200, {"Content-Type": "text/plain"});
+res.write("Usage: curl -i \\\n");
+res.write("   -H \"Content-Type: application/json\" \\\n");
+res.write("   -X POST -d '" + JSON.stringify(egPayload) + "' \\\n");
+res.write("   http://localhost:2001/reply");
+res.end();
 }
 
 // Send a JSON error to the browser.
 function error(res, message) {
-	res.json({
-		"status": "error",
-		"message": message
-	});
+res.json({
+	"status": "error",
+	"message": message
+});
 }
